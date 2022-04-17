5e701c24c63ba3d2e7892c7868f95442a695888c
Ville Hakulinen <ville.hakulinen@gmail.com>
2022-04-17 16:24:29 +0300

    scripts: update generate-bindings to use sponge

--- scripts/generate-bindings.sh
+++ scripts/generate-bindings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-nvim --api-info | cargo run --bin apigen functions > nvim-rs/src/gen.rs;
-nvim --api-info | cargo run --bin apigen uievents > nvim-rs/src/types/gen.rs
+nvim --api-info | cargo run --bin apigen functions | sponge nvim-rs/src/gen.rs;
+nvim --api-info | cargo run --bin apigen uievents | sponge nvim-rs/src/types/gen.rs
 
 cargo fmt
