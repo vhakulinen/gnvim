31996d999f73d06b4309a1337e757fd82dc00773
Ville Hakulinen <ville.hakulinen@gmail.com>
2022-04-18 21:40:47 +0300

    chore: restructure repo

--- scripts/generate-bindings.sh
+++ scripts/generate-bindings.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-nvim --api-info | cargo run --bin apigen functions | sponge nvim-rs/src/gen.rs;
-nvim --api-info | cargo run --bin apigen uievents | sponge nvim-rs/src/types/gen.rs
+nvim --api-info | cargo run --bin apigen functions | sponge lib/nvim-rs/src/gen.rs
+nvim --api-info | cargo run --bin apigen uievents | sponge lib/nvim-rs/src/types/gen.rs
 
 cargo fmt
