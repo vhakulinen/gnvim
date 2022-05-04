ad3876e744cc9ae1d18ea045dc3b91b7196d5d95
Ville Hakulinen <ville.hakulinen@gmail.com>
2022-05-04 19:21:21 +0300

    ui: add NON_UNIQUE app flag

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -16,8 +16,12 @@ pub const SCALE: f32 = pango::SCALE as f32;
 fn main() {
     gio::resources_register_include!("gnvim.gresource").expect("Failed to register resources.");
 
+    let mut flags = gio::ApplicationFlags::empty();
+    flags.insert(gio::ApplicationFlags::NON_UNIQUE);
+
     let app = Application::builder()
         .application_id("com.github.vhakulinen.gnvim")
+        .flags(flags)
         .build();
 
     app.connect_activate(build_ui);
