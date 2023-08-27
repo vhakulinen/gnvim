dfd07f7ccec3f1ae69286a7e266de16fc870bc12
Ville Hakulinen <ville.hakulinen@gmail.com>
2023-08-27 14:35:41 +0300

    ui: wrap messages when they're small enough

--- ui/src/components/message/mod.rs
+++ ui/src/components/message/mod.rs
@@ -24,10 +24,27 @@ mod imp {
     #[properties(wrapper_type = super::Message)]
     #[template(resource = "/com/github/vhakulinen/gnvim/message.ui")]
     pub struct Message {
-        #[property(get, set)]
+        #[property(get, set = Self::set_content)]
         pub content: RefCell<String>,
         #[property(get, set)]
         pub kind: RefCell<String>,
+
+        #[template_child(id = "content")]
+        pub content_label: TemplateChild<gtk::Label>,
+    }
+
+    impl Message {
+        fn set_content(&self, c: String) {
+            // If the content is _really_ long (e.g. `:highlight`), the label's
+            // performance plummets. To avoid such situtaion, add arbtitrary
+            // limit for wrapping.
+            if c.len() > 50_000 {
+                self.content_label.set_wrap(false);
+            } else {
+                self.content_label.set_wrap(true);
+            }
+            self.content.set(c)
+        }
     }
 
     #[glib::object_subclass]
