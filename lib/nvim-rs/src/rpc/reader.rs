b0f7b87364ea1a447333f205ae99286b4139a8f2
Ville Hakulinen <ville.hakulinen@gmail.com>
2024-08-13 16:39:29 +0300

    nvim-rs: use ring buffer for rpc buffering
    Current `Vec` backed buffering implementation causes a ton of extra
    allocations for each read message. Using a ring buffer (`VecDeque`)
    allows us to keep using the same allocated memory.
    
    `VecDeque`'s own `Read` implementation is "destructive", which we can
    use since we're likely to restart the read after failed deserialization.
    Fix this by implementing our own `Read`.

--- lib/nvim-rs/src/rpc/reader.rs
+++ lib/nvim-rs/src/rpc/reader.rs
@@ -1,8 +1,39 @@
-use std::io;
+use std::{collections::VecDeque, io};
 
 use futures::prelude::*;
 
 use super::message::Message;
+
+/// Cursor implementing non-destructive read for `VecDeque`.
+pub(crate) struct Cursor<'a> {
+    inner: &'a VecDeque<u8>,
+    pos: usize,
+}
+
+impl<'a> Cursor<'a> {
+    fn new(deque: &'a VecDeque<u8>) -> Self {
+        Self {
+            inner: deque,
+            pos: 0,
+        }
+    }
+}
+
+impl<'a> io::Read for Cursor<'a> {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        let start = self.pos.min(self.inner.len());
+
+        let (front, back) = self.inner.as_slices();
+        let n = if start < front.len() {
+            io::Read::read(&mut (&front[start..]), buf)?
+        } else {
+            io::Read::read(&mut (&back[(start - front.len())..]), buf)?
+        };
+
+        self.pos += n;
+        std::io::Result::Ok(n)
+    }
+}
 
 #[derive(Debug)]
 pub enum ReadError {
@@ -15,7 +46,7 @@ where
     R: AsyncRead + Unpin,
 {
     reader: futures::io::BufReader<R>,
-    buf: Vec<u8>,
+    buf: VecDeque<u8>,
 }
 
 impl<R> RpcReader<R>
@@ -25,7 +56,7 @@ where
     pub fn new(reader: R) -> Self {
         Self {
             reader: futures::io::BufReader::new(reader),
-            buf: Vec::new(),
+            buf: VecDeque::new(),
         }
     }
 
@@ -41,7 +72,7 @@ where
             ))),
             Ok(bytes) => {
                 // Add the available bytes to our buffer.
-                self.buf.extend_from_slice(bytes);
+                self.buf.extend(bytes);
 
                 // Tell the reader that we consumed the values.
                 let len = bytes.len();
@@ -54,14 +85,13 @@ where
 
     pub async fn recv(&mut self) -> Result<Message, ReadError> {
         loop {
-            let mut cursor = std::io::Cursor::new(&self.buf);
+            let mut cursor = Cursor::new(&self.buf);
 
             // Try decoding value from the buffer's current content.
             match rmp_serde::from_read::<_, Message>(&mut cursor) {
                 Ok(val) => {
                     // All good, there was enough data. Drop the read data.
-                    let at = cursor.position() as usize;
-                    self.buf = self.buf.split_off(at);
+                    self.buf.drain(..cursor.pos);
 
                     return Ok(val);
                 }
@@ -87,5 +117,34 @@ where
 {
     fn from(r: R) -> Self {
         RpcReader::new(r)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        collections::VecDeque,
+        io::{Read, Write},
+    };
+
+    use super::Cursor;
+
+    #[test]
+    fn test_cursor_read_wrapping() {
+        let mut buf = VecDeque::with_capacity(5);
+        buf.write_all(&[1, 2, 3, 4, 5]).unwrap();
+        buf.drain(..2);
+        buf.write_all(&[6]).unwrap();
+
+        // Buffer should be [6, <empty>, 3, 4, 5]
+        let (front, back) = buf.as_slices();
+        assert_eq!(&[3, 4, 5], front);
+        assert_eq!(&[6], back);
+
+        let mut cursor = Cursor::new(&buf);
+        let mut target = Vec::new();
+        let n = cursor.read_to_end(&mut target).unwrap();
+        assert_eq!(4, n);
+        assert_eq!(vec![3, 4, 5, 6], target);
     }
 }
